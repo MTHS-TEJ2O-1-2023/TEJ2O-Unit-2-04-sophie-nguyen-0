ad9f467373d8c49e11cd8f48c76fa771c4f27bf2
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-09-22 17:09:39 +0000

    python vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: sophie N
+Created on: Sep 2023
+This module is a Micro:bit MicroPython program that shows the temperature
 """
 
 from microbit import *
 
+display.clear
+display.show(Image.HEART)
 
-display.scroll("Hello, World!")
+temp = 0
+
+while True:
+    if button_a.is_pressed():
+        temp = temperature()
+        display.scroll("The temperature is " + str(temp) + " C")
