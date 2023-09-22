5870a9aa63a965c17039eb8f620a7093d719f890
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-09-22 12:52:04 -0400

    d

--- main.ts
+++ main.ts
@@ -9,10 +9,10 @@ basic.clearScreen()
 basic.showIcon(IconNames.Heart)
 basic.pause(1000)
 
-//variables
+// variables
 let temperature: number
 
 input.onButtonPressed(Button.A, function () {
   temperature = input.temperature()
-  basic.showString("The temperature is " + temperature.toString() + 'C')
+  basic.showString('The temperature is ' + temperature.toString() + 'C')
 })
